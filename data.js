82b9acb7d22625b0d4680fe2916754a07a3104b9
Brandom Ledesma <brandom.ledesma@hotmail.com>
2025-07-30 09:17:21 -0600

    Update invitation details and enhance clickable location styles with 3D shadow effect

--- data.js
+++ data.js
@@ -1,27 +1,29 @@
 /* data.js */
 export const invitacion = {
-  festejada: "Vanessa Corpus",
-  medallionName: "Nayeli Nicol",
+  festejada: "Abril",
+  medallionName: "Abril",
   fecha: "2026-01-01",
-  hora: "16:30",
+  hora: "19:00",
   mensajeBienvenida:
     "Hay eventos en la vida que marcan un antes y un después, y para mí, mis quince años son uno de ellos. Me encantaría compartir este momento tan especial contigo.",
   agradecimiento:
     "Gracias, papás y queridos padrinos, por hacer este sueño realidad y por su amor incondicional.",
-  padres: ["Vanessa Corpus", "Carlos Aranda"],
-  padrinos: ["Karen Corpus", "Hugo Lizagarra"],
+  padres: ["Erika B.", "Lama Delgadillo"],
+  padrinos: ["Guadalupe Francisco Reyes", "Sergio Portillo "],
   programa: [
     {
-      evento: "Misa de Acción de Gracias",
-      hora: "16:30",
-      lugar: "Parroquia de San Miguel Arcángel",
+      evento:
+        "Parroquia de la santísima Trinidad de Nuestra Señora del refugio",
+      hora: "19:00",
+      lugar: "francisco Tamagno 254, colonia Peralvillo, CP 06220",
       coords: [19.4326, -99.1332],
       icono: "church",
     },
     {
-      evento: "Recepción y Banquete",
+      evento: "Royal Rizado Lindavista",
       hora: "20:00",
-      lugar: "Salón de Eventos 'El Castillo Encantado'",
+      lugar:
+        "av insurgentes 1970, colonia lindavista , CP 07300, alcaldía Gustavo a Madero",
       coords: [19.4, -99.15],
       icono: "celebration",
     },
@@ -36,5 +38,6 @@ export const invitacion = {
         "Si deseas hacer un regalo, te agradeceríamos que lo hicieras en efectivo",
     },
   ],
-  rsvpLink: "https://wa.me/5215555555555",
+  rsvpLink:
+    "https://wa.me/5585420616?text=Hola%20Abril,%20confirmo%20mi%20asistencia%20a%20tus%20XV%20Años.%20Gracias%20por%20la%20invitación!",
 };
